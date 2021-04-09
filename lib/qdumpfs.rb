700c6c84a1f8b3cde2d3d680deb8b09f27bae377
src <src@srcw.net>
2021-04-09 22:14:28 +0900

    change wprintf to log

--- lib/qdumpfs.rb
+++ lib/qdumpfs.rb
@@ -180,7 +180,7 @@ module Qdumpfs
           update_file(s, l, t)
           dirs[t] = File.stat(s) if File.ftype(s) == "directory"
         rescue => e
-          wprintf("%s: %s", src, e.message)
+          log("%s: %s", src, e.message)
           next
         end
       end
@@ -215,7 +215,7 @@ module Qdumpfs
           chown_if_root(type, s, t)
           dirs[t] = File.stat(s) if File.ftype(s) == "directory"
         rescue => e
-          wprintf("%s: %s", s, e.message)
+          log("%s: %s", src, e.message)          
           next
         end
       end
